3d667b0c327a4c2940595fde0284a7796097b772
Chizzy <chizoba.princess@gmail.com>
2020-01-24 12:51:50 -0500

    added error handling/exceptions

--- src/Model/Subtask.php
+++ src/Model/Subtask.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Model;
+use App\Exception\ApiException;
 
 class Subtask
 {
@@ -12,26 +13,43 @@ class Subtask
 
     public function getSubtasksByTaskId($task_id)
     {
+        if (empty($task_id)) {
+            throw new ApiException(ApiException::TASK_INFO_REQUIRED);
+        }
         $statement = $this->database->prepare(
             'SELECT * FROM subtasks WHERE task_id = :task_id ORDER BY id'
         );
         $statement->bindParam('task_id', $task_id);
         $statement->execute();
-        return $statement->fetchAll();
+        $subtasks = $statement->fetchAll();
+        if (empty($subtasks)) {
+            throw new ApiException(ApiException::SUBTASK_NOT_FOUND, 404);
+        }
+        return $subtasks;
     }
 
     public function getSubtask($subtask_id)
     {
+        if (empty($subtask_id)) {
+            throw new ApiException(ApiException::SUBTASK_INFO_REQUIRED);
+        }
         $statement = $this->database->prepare(
             'SELECT * FROM subtasks WHERE id = :id'
         );
         $statement->bindParam('id', $subtask_id);
         $statement->execute();
-        return $statement->fetch();
+        $subtask = $statement->fetch();
+        if (empty($subtask)) {
+            throw new ApiException(ApiException::SUBTASK_NOT_FOUND, 404);
+        }
+        return $subtask;
     }
 
     public function createSubtask($data)
     {
+        if (empty($data['name']) || empty($data['status']) || empty($data['task_id'])) {
+            throw new ApiException(ApiException::SUBTASK_INFO_REQUIRED);
+        }
         $statement = $this->database->prepare(
             'INSERT INTO subtasks (name, status, task_id) VALUES (:name, :status, :task_id)'
         );
@@ -39,11 +57,17 @@ class Subtask
         $statement->bindParam('status', $data['status']);
         $statement->bindParam('task_id', $data['task_id']);
         $statement->execute();
+        if ($statement->rowCount() < 1) {
+            throw new ApiException(ApiException::SUBTASK_CREATION_FAILED);
+        }
         return $this->getSubtask($this->database->lastInsertId());
     }
 
     public function updateSubtask($data)
     {
+        if (empty($data['name']) || empty($data['status']) || empty($data['subtask_id'])) {
+            throw new ApiException(ApiException::SUBTASK_INFO_REQUIRED);
+        }
         $statement = $this->database->prepare(
             'UPDATE subtasks SET name = :name, status = :status WHERE id = :id'
         );
@@ -51,16 +75,23 @@ class Subtask
         $statement->bindParam('status', $data['status']);
         $statement->bindParam('id', $data['subtask_id']);
         $statement->execute();
+        if ($statement->rowCount() < 1) {
+            throw new ApiException(ApiException::SUBTASK_UPDATE_FAILED);
+        }
         return $this->getSubtask($data['subtask_id']);
     }
 
     public function deleteSubtask($subtask_id)
     {
+        $this->getSubtask($subtask_id);
         $statement = $this->database->prepare(
             'DELETE FROM subtasks WHERE id = :id'
         );
         $statement->bindParam('id', $subtask_id);
         $statement->execute();
+        if ($statement->rowCount() < 1) {
+            throw new ApiException(ApiException::SUBTASK_DELETE_FAILED);
+        }
         return ['message' => 'The subtask was deleted.'];
     }
 }
--- src/Model/Task.php
+++ src/Model/Task.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Model;
+use App\Exception\ApiException;
 
 class Task
 {
@@ -15,7 +16,11 @@ class Task
         $statement = $this->database->query(
             'SELECT * FROM tasks ORDER BY id'
         );
-        return $statement->fetchAll();
+        $tasks = $statement->fetchAll();
+        if (empty($tasks)) {
+            throw new ApiException(ApiException::TASK_NOT_FOUND, 404);
+        }
+        return $tasks;
     }
 
     public function getTask($task_id)
@@ -25,22 +30,35 @@ class Task
         );
         $statement->bindParam('id', $task_id);
         $statement->execute();
-        return $statement->fetch();
+        $task = $statement->fetch();
+        if (empty($task)) {
+            throw new ApiException(ApiException::TASK_NOT_FOUND, 404);
+        }
+        return $task;
     }
 
     public function createTask($data)
     {
+        if (empty($data['task']) || empty($data['status'])) {
+            throw new ApiException(ApiException::TASK_INFO_REQUIRED);
+        }
         $statement = $this->database->prepare(
             'INSERT INTO tasks (task, status) VALUES (:task, :status)'
         );
         $statement->bindParam('task', $data['task']);
         $statement->bindParam('status', $data['status']);
         $statement->execute();
+        if ($statement->rowCount() < 1) {
+            throw new ApiException(ApiException::TASK_CREATION_FAILED);
+        }
         return $this->getTask($this->database->lastInsertId());
     }
 
     public function updateTask($data)
     {
+        if (empty($data['task']) || empty($data['status']) || empty($data['task_id'])) {
+            throw new ApiException(ApiException::TASK_INFO_REQUIRED);
+        }
         $statement = $this->database->prepare(
             'UPDATE tasks SET task = :task, status = :status WHERE id = :id'
         );
@@ -48,16 +66,23 @@ class Task
         $statement->bindParam('status', $data['status']);
         $statement->bindParam('id', $data['task_id']);
         $statement->execute();
+        if ($statement->rowCount() < 1) {
+            throw new ApiException(ApiException::TASK_UPDATE_FAILED);
+        }
         return $this->getTask($data['task_id']);
     }
 
     public function deleteTask($task_id)
     {
+        $this->getTask($task_id);
         $statement = $this->database->prepare(
             'DELETE FROM tasks WHERE id = :id'
         );
         $statement->bindParam('id', $task_id);
         $statement->execute();
+        if ($statement->rowCount() < 1) {
+            throw new ApiException(ApiException::TASK_DELETE_FAILED);
+        }
         return ['message' => 'The task was deleted.'];
     }
 }
